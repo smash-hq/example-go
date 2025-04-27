d7efc2e07754e8747a8100dd681f7dcabaef5d3f
Epiphany <smash.qi.huang@gmail.com>
2025-04-27 11:00:44 +0800

    init

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	// 打开/创建日志文件
+	logFile, err := os.OpenFile("/var/log/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Printf("Failed to open log file: %v\n", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
 
-	for i := 0; i < 50; i++ {
+	// 将标准输出重定向到日志文件
+	os.Stdout = logFile
+	i := 0
+	for {
 		fmt.Printf("%d--> Hello World\n", i+1)
 		time.Sleep(1 * time.Second)
+		i++
 	}
-	//os.Exit(0)
-	panic("Panic")
+	select {}
 }
