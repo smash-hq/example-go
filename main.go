119612e62395802d6c8cc28fc3695e2af23f01fd
Epiphany <smash.qi.huang@gmail.com>
2025-05-08 14:24:10 +0800

    refactor: 重构项目以实现基本的 HTTP 服务器功能
    - 移除了 NATS 相关的依赖和代码
    - 添加了 HTTP 服务器的基本实现
    - 新增了 /hello 路径的处理函数
    - 更新了 main 函数以启动 HTTP 服务器

--- main.go
+++ main.go
@@ -2,44 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/nats-io/nats.go"
-	"log"
-	"time"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request on /hello")
+	fmt.Fprintln(w, "hello")
+}
+
 func main() {
-	// 连接到 NATS 服务器
-	natsURL := "nats://nats.nats.svc.cluster.local:4222"
-	nc, err := nats.Connect(natsURL)
+	http.HandleFunc("/hello", helloHandler)
+
+	port := ":8849"
+	fmt.Printf("Server listening on port %s\n", port)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	defer nc.Drain()
-
-	// 设置回调接收结果
-	_, err = nc.Subscribe("job.A.result", func(m *nats.Msg) {
-		fmt.Printf("Job A received result: %s\n", string(m.Data))
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	i := 0
-	for {
-		fmt.Printf("Iteration %d\n", i)
-		// 发布请求给 Job B，回调 `job.A.result` 用于接收结果
-		err = nc.PublishMsg(&nats.Msg{
-			Subject: "job.B.run",                                      // 请求发送到 job.B.run 主题
-			Reply:   "job.A.result",                                   // 设置回调地址为 job.A.result
-			Data:    []byte(fmt.Sprintf("Hello, Job B!, I am %d", i)), // 请求的内容
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		time.Sleep(2 * time.Second)
-		i++
-	}
-
-	// 保持连接，以便接收回调结果
-	select {}
 }
