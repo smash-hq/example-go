c129d30d82060d38ded0b6a643151496d43f0aac
Epiphany <smash.qi.huang@gmail.com>
2025-05-08 11:07:49 +0800

    init2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/nats-io/nats.go"
 	"log"
+	"time"
 )
 
 func main() {
@@ -15,18 +16,30 @@ func main() {
 	}
 	defer nc.Drain()
 
-	// 订阅 `job.B.run` 主题
-	nc.Subscribe("job.B.run", func(m *nats.Msg) {
-		fmt.Printf("Job B received: %s\n", string(m.Data))
-
-		// 模拟处理，并将结果发送回调用者（Job A）
-		result := []byte("Job B completed successfully")
-		if m.Reply != "" {
-			// 回复到 Job A 的回调地址
-			nc.Publish(m.Reply, result)
-		}
+	// 设置回调接收结果
+	_, err = nc.Subscribe("job.A.result", func(m *nats.Msg) {
+		fmt.Printf("Job A received result: %s\n", string(m.Data))
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// 保持运行，等待接收消息
+	i := 0
+	for {
+		fmt.Printf("Iteration %d\n", i)
+		// 发布请求给 Job B，回调 `job.A.result` 用于接收结果
+		err = nc.PublishMsg(&nats.Msg{
+			Subject: "job.B.run",             // 请求发送到 job.B.run 主题
+			Reply:   "job.A.result",          // 设置回调地址为 job.A.result
+			Data:    []byte("Hello, Job B!"), // 请求的内容
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(1 * time.Second)
+		i++
+	}
+
+	// 保持连接，以便接收回调结果
 	select {}
 }
