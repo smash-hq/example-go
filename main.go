339c6f252445ab5a67822379a16886e918dbdc85
Epiphany <smash.qi.huang@gmail.com>
2025-05-29 10:49:47 +0800

    refactor(main): 重构 main函数
    - 移除了与 resolv.conf 相关的代码- 删除了循环打印日志的代码
    - 简化为只打印 "Hello World!" 和 "Goodbye World!"

--- main.go
+++ main.go
@@ -1,46 +1,12 @@
 package main
 
 import (
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"os"
+	"log"
 	"time"
 )
 
-const resolvPath = "/etc/resolv.conf"
-
 func main() {
-	// Step 1: 读取原始内容
-	original, err := os.ReadFile(resolvPath)
-	if err != nil {
-		fmt.Println(fmt.Errorf("读取失败:%s", err))
-	}
-
-	fmt.Println("===== 原始 /etc/resolv.conf =====")
-	fmt.Println(string(original))
-
-	// Step 2: 构造新的内容（添加一个自定义 DNS）
-	newContent := string(original) + "\nnameserver 1.1.1.1\n"
-
-	// Step 3: 写入新内容
-	err = os.WriteFile(resolvPath, []byte(newContent), 0644)
-	if err != nil {
-		fmt.Println(fmt.Errorf("写入失败:%s", err))
-	}
-
-	// Step 4: 读取并打印修改后的内容
-	modified, err := os.ReadFile(resolvPath)
-	if err != nil {
-		fmt.Println(fmt.Errorf("再次读取失败:%s", err))
-	}
-
-	fmt.Println("===== 修改后 /etc/resolv.conf =====")
-	fmt.Println(string(modified))
-	log.Println("test get k8s info")
-	i := 1
-	for {
-		log.Printf("hello world-->%d", i)
-		i++
-		time.Sleep(2 * time.Second)
-	}
+	log.Println("Hello World!")
+	time.Sleep(2 * time.Second)
+	log.Println("Goodbye World!")
 }
